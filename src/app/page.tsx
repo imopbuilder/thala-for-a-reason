9bf75d227c2c7c8d38845b60190e2f32d399a335
imopbuilder <opbuilder9@gmail.com>
2023-12-23 21:49:12 +0530

    feat: add header to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 export default function Home() {
 	return (
-		<main>
-			<p>hello world</p>
-		</main>
+		<Fragment>
+			<header className='border-b'>
+				<div className='h-14 flex items-center justify-between max-w-maxi mx-auto'>
+					<div>
+						<Link href={'/'} className='font-medium'>
+							Thala for a reason
+						</Link>
+					</div>
+					<div>
+						<Button size='sm' asChild>
+							<Link href='/'>Support Project</Link>
+						</Button>
+					</div>
+				</div>
+			</header>
+			<main>
+				<p>hello world</p>
+			</main>
+		</Fragment>
 	);
 }
---
+++ src/components/pages/home/client.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export function HomeClient() {
+	return (
+		<div>
+			<p>home client</p>
+		</div>
+	);
+}
